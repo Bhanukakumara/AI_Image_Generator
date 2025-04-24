d8ef903be90bcb0243eb2f2a48f11f4882b77b31
Bhanukakumara <bhanukarasika@gmail.com>
2025-04-25 00:52:42 +0530

    Toggle Button Update

--- script/main.js
+++ script/main.js
@@ -16,3 +16,5 @@ const toggleTheme = () => {
   const isDarkTheme = document.body.classList.toggle("dark-theme");
   themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
 };
+
+themeToggle.addEventListener("click", toggleTheme);
