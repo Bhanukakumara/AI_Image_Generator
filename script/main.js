74730b2dbea8cb3fdb9b2331605cf93d63d1474d
Bhanukakumara <bhanukarasika@gmail.com>
2025-04-25 00:51:14 +0530

    JS Variables

---
+++ script/main.js
@@ -0,0 +1,13 @@
+// Theme Toggle
+const themeToggle = document.querySelector(".theme-toggle");
+const promptForm = document.querySelector(".prompt-form");
+const promptInput = document.querySelector(".prompt-input");
+const promptBtn = document.querySelector(".prompt-btn");
+
+const modelSelect = document.querySelector("#model-select");
+const countSelect = document.querySelector("#count-select");
+const ratioSelect = document.querySelector("#ratio-select");
+
+const gridGallery = document.querySelector(".gallery-grid");
+
+const API_KEY = "";
